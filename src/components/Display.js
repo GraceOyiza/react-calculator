2cafc5389bcd6d472015a549ffe3063f86b155cb
GraceOyiza <graceoyiza13@gmail.com>
2021-07-16 13:44:38 +0100

    Add calculation as incoming props

---
+++ src/components/Display.js
@@ -0,0 +1,8 @@
+export default function Display({ memorized, current }) {
+  return (
+    <div className="display">
+      <div className="display_upper">{memorized}</div>
+      <div className="display_lower">{current}</div>
+    </div>
+  );
+}
