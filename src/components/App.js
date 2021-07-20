14c7b1fdad0850f7c191511099627a3b4f287597
GraceOyiza <graceoyiza13@gmail.com>
2021-07-20 11:19:03 +0100

    Remove array and import buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,13 @@
 import ButtonPannel from './ButtonPanel';
 import Display from './Display';
-import Button from './Button';
 
 function App() {
-  const buttons = [
-    'AC',
-    'X²',
-    '%',
-    '/',
-    '9',
-    '8',
-    '7',
-    '*',
-    '6',
-    '5',
-    '4',
-    '+',
-    '3',
-    '2',
-    '1',
-    '-',
-    '0',
-    '.',
-    '+/-',
-    '=',
-  ];
   return (
     <>
       <div className="App">
         <div className="Appcalculator">
           <Display />
-          <ButtonPannel>
-            {buttons.map(button => (
-              <Button key={button} name={button} />
-            ))}
-          </ButtonPannel>
+          <ButtonPannel />
         </div>
       </div>
     </>
