37fa31668b724d4c0898a0a520a5b6a647117a71
GraceOyiza <graceoyiza13@gmail.com>
2021-07-22 15:42:26 +0100

    Add number functionality

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,9 +1,23 @@
 import operate from './operate';
 
-const calculate = () => {
+const calculate = (calculator, btn) => {
+  let { total, next, operator, prev } = calculator;
   const numbers = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
 
   const operators = ['/', '*', '+', '-'];
+
+  if (numbers.includes(btn)) {
+    total = null;
+    if (!prev || prev === '0') {
+      prev = btn;
+    } else if (prev && !operator) {
+      prev += btn;
+    } else if ((operator && !next) || (next && next[0] === '0')) {
+      next = btn;
+    } else if (operator && next) {
+      next += btn;
+    }
+  }
 };
 
 export default calculate;
