f48a67bce4082b83ae0f4780f479c52cb87a41a6
GraceOyiza <graceoyiza13@gmail.com>
2021-07-22 15:28:03 +0100

    Switch method for operator

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,21 @@
 import Big from 'big.js';
+
+export default (Number1, Number2, operator) => {
+  const firstNumber = new Big(Number1);
+  const secondNumber = Number2 && new Big(Number2);
+
+  switch (operator) {
+    case '/':
+      return firstNumber.div(secondNumber);
+    case '*':
+      return firstNumber.mul(secondNumber);
+    case '+':
+      return firstNumber.add(secondNumber);
+    case 'X²':
+      return firstNumber.pow(2);
+    case '%':
+      return firstNumber.div(100).toString();
+    default:
+      return firstNumber.sub(secondNumber);
+  }
+};
