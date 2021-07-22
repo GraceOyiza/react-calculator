935a2d33ec1e187d28179b8399e2d9816dc10f37
GraceOyiza <graceoyiza13@gmail.com>
2021-07-22 15:34:57 +0100

    Import operate

---
+++ src/logic/calculate.js
@@ -0,0 +1,9 @@
+import operate from './operate';
+
+const calculate = () => {
+  const numbers = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+
+  const operators = ['/', '*', '+', '-'];
+};
+
+export default calculate;
