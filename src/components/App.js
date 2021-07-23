c346382d806f0516467dad2208482ddf134bdb36
GraceOyiza <graceoyiza13@gmail.com>
2021-07-23 21:58:06 +0100

    Declare buttons values

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,28 @@
 import ButtonPannel from './ButtonPanel';
 import Display from './Display';
 
+const buttons = [
+  'AC',
+  'X²',
+  '%',
+  '/',
+  '9',
+  '8',
+  '7',
+  '*',
+  '6',
+  '5',
+  '4',
+  '+',
+  '3',
+  '2',
+  '1',
+  '-',
+  '0',
+  '.',
+  '+/-',
+  '=',
+];
 function App() {
   return (
     <>
