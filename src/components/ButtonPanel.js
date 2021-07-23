87d9b233644b1a9427abfcf96eb5ab60e5639909
GraceOyiza <graceoyiza13@gmail.com>
2021-07-23 23:00:41 +0100

    Remove button panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,29 +1,6 @@
 import Button from './Button';
 
 export default function ButtonPanel() {
-  const buttons = [
-    'AC',
-    'X²',
-    '%',
-    '/',
-    '9',
-    '8',
-    '7',
-    '*',
-    '6',
-    '5',
-    '4',
-    '+',
-    '3',
-    '2',
-    '1',
-    '-',
-    '0',
-    '.',
-    '+/-',
-    '=',
-  ];
-
   return (
     <div className="button_panel">
       {buttons.map(button => (
