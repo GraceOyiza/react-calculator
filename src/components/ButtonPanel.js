4102b03505df5ed0768bf9a6c8655e0849d01240
GraceOyiza <graceoyiza13@gmail.com>
2021-07-24 11:54:20 +0100

    Import proptypes

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,5 +1,13 @@
 import Button from './Button';
+import PropTypes from 'prop-types';
 
 export default function ButtonPanel({ children }) {
   return <div className="button_panel">{children}</div>;
 }
+
+ButtonPanel.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
