5962bb1a0fd332922b05d6c7e7fa10353dc7fb1e
GraceOyiza <graceoyiza13@gmail.com>
2021-07-27 22:45:39 +0100

    Remove square case

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,7 +3,9 @@ import Big from 'big.js';
 export default (Number1, Number2, operator) => {
   const firstNumber = new Big(Number1);
   const secondNumber = Number2 && new Big(Number2);
-
+  if (operator === '/' && parseInt(secondNumber, 10) === 0) {
+    return 'Cant divide by 0';
+  }
   switch (operator) {
     case '/':
       return firstNumber.div(secondNumber);
@@ -11,8 +13,6 @@ export default (Number1, Number2, operator) => {
       return firstNumber.mul(secondNumber);
     case '+':
       return firstNumber.add(secondNumber);
-    case 'X²':
-      return firstNumber.pow(2);
     case '%':
       return firstNumber.div(100).toString();
     default:
